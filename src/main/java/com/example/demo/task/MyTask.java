f05554ff56c5569bcc32faeb4c11fb5480b40ee9
66coder66 <390388252@qq.com>
2018-07-22 14:58:21 +0800

    add log file

---
+++ src/main/java/com/example/demo/task/MyTask.java
@@ -0,0 +1,16 @@
+package com.example.demo.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyTask {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@Scheduled(cron = "0/10 * * * * ?") // 每5秒执行一次
+	public void scheduler() {
+	   logger.info(">>>>>>>>>>>>> scheduled test... ");
+	}
+}
