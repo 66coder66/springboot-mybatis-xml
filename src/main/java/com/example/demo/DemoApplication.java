bd40340373b707d43ddb666e57381c620479a1ae
66coder66 <390388252@qq.com>
2018-07-22 15:12:40 +0800

    add task

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @ImportResource("classpath:transaction.xml")
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling//增加支持定时任务的注解
 @MapperScan("com.dsh.demo.mapper")
 public class DemoApplication {
 	
